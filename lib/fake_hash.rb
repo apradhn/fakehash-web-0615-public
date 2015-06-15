37b5cafb8fc5c2867a0db3ae077c7dbc6dfa3886
apradhn <abhijit.pradhan6@gmail.com>
2015-06-15 10:42:43 -0400

    all done

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,40 @@
-class FakeHashWrapper
+require "pry"
 
-  # write code here
+class FakeHashWrapper
+  attr_accessor :hash
+
+  def initialize
+    @hash = {}
+    @keys = []
+  end
+
+  def []=(key, value)
+    @hash[key] = value
+    @keys << key
+  end
+
+  def [](key)
+    if @keys.include?(key)
+      hash[key]
+    elsif @keys.include?(key.to_s)
+      hash[key.to_s]
+    elsif @keys.include?(key.to_sym)
+      hash[key.to_sym]
+    end
+  end
+
+  def keys
+    @keys.sort{|a, b| b <=> a}
+  end
+
+  def first_letter(letter)
+    @hash.select {|key, value| key.to_s[0] == letter}.collect{|key, value| value}
+  end
+
+  def each
+    @hash.each do |key, value|
+      yield(key, value)
+    end
+  end
 
 end
